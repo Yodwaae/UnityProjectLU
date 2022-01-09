a3fb477d6038ac2d4957b6dfc27ed32bf32f8ba9
Firrow <73218766+Firrow@users.noreply.github.com>
2022-01-09 19:57:38 +0100

    Options fonctionnent (en théorie)
    Les changement de résolution, le plein écran et la gestion du son fonctionne
    PROBLEME : test du changement de résolution avec un build qui a marché mais qui m'a bloqué. J'ai effacé le build pour en refaire et ils étaient tous dans la résolution que j'avais choisi pour le test donc je suis bloquée X(

--- Assets/Scripts/Menus/SettingsMenu.cs
+++ Assets/Scripts/Menus/SettingsMenu.cs
@@ -29,7 +29,11 @@ public class SettingsMenu : MonoBehaviour
         Screen.fullScreen = isFullScreen;
     }
 
-
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
 
 
@@ -39,6 +43,8 @@ public class SettingsMenu : MonoBehaviour
             resolutions = Screen.resolutions;
             resolutionsDropdown.ClearOptions();
 
+            int currentResolutionIndex = 0;
+
             List<string> options = new List<string>();
 
             for (int i = 0; i < resolutions.Length; i++)
@@ -46,9 +52,19 @@ public class SettingsMenu : MonoBehaviour
                 //on créer une string contenant la résolution de l'écran du joueur car le Dropdown ne peut contenir que des strings
                 string option = resolutions[i].width + "x" + resolutions[i].height;
                 options.Add(option);
+
+                //vérification : est ce que la résolution actuelle correspond à la taille de l'écran du joueur
+                if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+                {
+                    currentResolutionIndex = i;
+                }
             }
 
             resolutionsDropdown.AddOptions(options); //on envoie la liste de toutes les options de résolution à la liste déroulante du menu des Settings
+
+            //on lance la meilleure résolution possible
+            resolutionsDropdown.value = currentResolutionIndex;
+            resolutionsDropdown.RefreshShownValue(); //raffraichit l'élément sélectionner dans la liste déroulante du menu des options
         //----------------------------------------------------------------------------------------------------------------------------------------------------
     }
 }
