79007658621ac39ab3bd13c495e7841ee1124d89
Firrow <73218766+Firrow@users.noreply.github.com>
2022-01-10 22:11:44 +0100

    BUG ENCORE PRESENT Commit de petits changement

--- Assets/Scripts/Camera/Follow.cs
+++ Assets/Scripts/Camera/Follow.cs
@@ -22,7 +22,6 @@ public class Follow : MonoBehaviour
          le -10 en z sert à garder la caméra à la bonne hauteur pour voir le joueur*/
         transform.position = new Vector3(playerPos.transform.position.x, playerPos.transform.position.y, -10) ;
         float ratio = (cam.orthographicSize * cam.scaledPixelWidth) / cam.scaledPixelHeight;
-        Debug.Log(ratio);
         /*Limite du terrain, permet de décentrer la camera du joueur quand il arrive au bord du terrain
          evitant que la camera filme du vide qui ne fait pas partie de l'espace de jeu*/
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -1.5f, 1.3f), Mathf.Clamp(transform.position.y, -5f, 5f), -10);
