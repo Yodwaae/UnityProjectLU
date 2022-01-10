a703f13d5a377f732cac31eb8de9985a8c7fec97
Yodwaae <48288304+Yodwaae@users.noreply.github.com>
2022-01-10 13:15:04 +0100

    Test Caméra

--- Assets/Scripts/Camera/Follow.cs
+++ Assets/Scripts/Camera/Follow.cs
@@ -6,22 +6,25 @@ public class Follow : MonoBehaviour
 {
 
     private Transform playerPos;//Variable qui va servir à stocker la pos du joueur
+    private Camera cam;
 
     private void Awake()
     {
         //Trouve l'objet joueur et récupère son composant Transform
         playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        cam = GetComponent<Camera>();
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         /*Fait suivre le mouvement du joueur à la caméra, 
          le -10 en z sert à garder la caméra à la bonne hauteur pour voir le joueur*/
         transform.position = new Vector3(playerPos.transform.position.x, playerPos.transform.position.y, -10) ;
-
+        float ratio = (cam.orthographicSize * cam.scaledPixelWidth) / cam.scaledPixelHeight;
+        Debug.Log(ratio);
         /*Limite du terrain, permet de décentrer la camera du joueur quand il arrive au bord du terrain
          evitant que la camera filme du vide qui ne fait pas partie de l'espace de jeu*/
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -4.41f, 4.45f), Mathf.Clamp(transform.position.y, -4.98f, 5.06f), -10);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -1.5f, 1.3f), Mathf.Clamp(transform.position.y, -5f, 5f), -10);
     }
 }
