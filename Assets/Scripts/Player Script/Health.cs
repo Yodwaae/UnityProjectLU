538b40575450ed81e20e0d1d01d3596a17f29114
Firrow <73218766+Firrow@users.noreply.github.com>
2022-01-10 11:45:16 +0100

    Changement taille halo
    Change la taille du halo en fonction du type de coup pris par le joueur (ennemi ou allié)

--- Assets/Scripts/Player Script/Health.cs
+++ Assets/Scripts/Player Script/Health.cs
@@ -6,8 +6,9 @@ public class Health : MonoBehaviour
 {
     [SerializeField]
     private int vie;//Crée la variable de vie du joueur
-    public GameObject gManager;//Crée la référence au GameManager NE FONCTIONNE PAS
-    private SpawnAllies allySpawner;//Crée la référence au spawner allié NE FONCTIONNE PAS
+    public GameObject gManager;//Créer la référence au GameManager
+    private SpawnAllies allySpawner;//Crée la référence au spawner allié
+    public UnityEngine.Experimental.Rendering.Universal.Light2D lightPlayer;
 
     private void Awake()
     {
@@ -24,19 +25,25 @@ public class Health : MonoBehaviour
             //Important de détruire l'objet tout de suite ou le joueur perd plusieurs hp avec le même ennemi
             //Pour l'animation on pourra donc juste instantiate un objet qui est une animation , qui se détruit elle même une fois finis
             //(mettre un Destroy(gameObject, XXf) dans le Start/Awake)
-            allySpawner.UpdateSpawnerA();////Apelle l'update Spawner quand le joueur perd de la vie plutôt quà chaque frame PAS FONCTIONNER
+            allySpawner.UpdateSpawnerA();////Apelle l'update Spawner quand le joueur perd de la vie plutôt quà chaque frame
+            //diminue la taille du halo lorsque le joueur a touché un ennemi
+            lightPlayer.pointLightOuterRadius -= 1;
+            lightPlayer.pointLightInnerRadius -= 1;
         }
         else if (collision.gameObject.tag == "Ally")//Si la collision est un allié simple
         {
             vie++;//Gagne un point de vie
             Destroy(collision.gameObject);//détruit l'allié
-            allySpawner.UpdateSpawnerA();////Apelle l'update Spawner quand le joueur gagne de la vie plutôt quà chaque frame PAS FONCTIONNER
+            allySpawner.UpdateSpawnerA();////Apelle l'update Spawner quand le joueur gagne de la vie plutôt quà chaque frame
+            //augmente la taille du halo lorsque le joueur a touché un allié
+            lightPlayer.pointLightOuterRadius += 1;
+            lightPlayer.pointLightInnerRadius += 1;
         }
         else if (collision.gameObject.tag == "Ally2")//Si la collision est un allié "double"
         { 
             vie = vie + 2;//Gagne deux points de vie
             Destroy(collision.gameObject);//détruit l'allié
-            allySpawner.UpdateSpawnerA();////Apelle l'update Spawner quand le joueur gagne de la vie plutôt quà chaque frame PAS FONCTIONNER
+            allySpawner.UpdateSpawnerA();////Apelle l'update Spawner quand le joueur gagne de la vie plutôt quà chaque frame
         }
     }
 
