ba8d632d70a80e33b9d98a2e4f8e34fa1f3a863a
Firrow <73218766+Firrow@users.noreply.github.com>
2022-01-19 18:18:42 +0100

    Mise en place des textes écran gameOver

--- Assets/Scripts/Interface/GameOverManager.cs
+++ Assets/Scripts/Interface/GameOverManager.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverManager : MonoBehaviour
 {
     public GameObject gameOverUI;
     public static GameOverManager instance;
+
+    public Text time;
+    public Text scoreKillAllies;
+    public Text scoreKillEnemies;
 
     private void Awake()
     {
